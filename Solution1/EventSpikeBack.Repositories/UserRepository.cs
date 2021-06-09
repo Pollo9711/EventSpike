982bdb3cae6a17caf680debf0b6d167526103d29
Andrea Pollini <andrea9711@hotmail.it>
2021-06-09 09:14:56 +0200

    fhfhfh

---
+++ Solution1/EventSpikeBack.Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using EventSpikeBack.Domain;
+using EventSpikeBack.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using EventSpikeBack.Context;
+using EventSpikeBack.Context.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSpikeBack.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public readonly EventSpikeBackDbContext _context;
+        public readonly IMapper _mapper;
+
+        public UserRepository(EventSpikeBackDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void Add(UserDomain user)
+        {
+            var userEntity = _mapper.Map<UserEntity>(user);
+            _context.User.Add(userEntity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var idEntity = _context.User.FirstOrDefault( u =>u.Id == id);
+            _context.User.Remove(idEntity);
+            _context.SaveChanges();
+
+        }
+
+        public UserDomain Get(int id)
+        {
+            
+        }
+
+        public List<UserDomain> GetAll()
+        {
+            
+        }
+
+        public void Update(UserDomain user)
+        {
+            
+        }
+    }
+}
